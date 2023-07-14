47ecfe99e3544c920fca1a962eb26d6e049238ba
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-07-14 16:39:06 +0530

    sign in success

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,22 +3,24 @@
 class Users::SessionsController < Devise::SessionsController
   
   def new
-   @user = User::Session.new
+   @user = User.new
   end
   
   def create
-    debugger
-    @user = User::Session.new(user_params)
-    if @user.save
-      render json: {message: 'User signed in'}
-    else
-      render json: {message: 'User not create'}
+     @user = User.find_by(email: user_params[:email])
+    
+     if @user && @user.valid_password?(user_params[:password])
+      sign_in(@user)
+      render json: { message: 'User signed in' }
+     else
+      render json: { message: 'Invalid email or password' }
+     end
     end
-  end
 
   private
+  
   def user_params
    params.require(:user).permit(:email,:password,:password_confirmation)
   end
-
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations:'user/registrations'
+    registrations:'users/registrations'
   }
 end
