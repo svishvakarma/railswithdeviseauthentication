f1cc746d6bbd54cdc5658e18626443a264f2889d
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-07-14 16:51:08 +0530

    added authentication controlller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::API
 		devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
 		devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
 	end
+	
+	def not_found
+    render json: { error: 'not_found' }
+  end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = JsonWebToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
